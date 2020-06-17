dc057e578494d7ad3ee4524ecb5349a4d021a277
paulo70 <paulofrontend@gmail.com>
2020-06-17 19:46:16 -0300

    initial config react and others librarys

---
+++ src/coin.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './coin.css';
+
+function Coin() {
+  return (
+    <h1>Coin Convertor</h1>
+  );
+}
+
+export default Coin;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Coin from './coin';
 import * as serviceWorker from './serviceWorker';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Coin />
   </React.StrictMode>,
   document.getElementById('root')
 );
